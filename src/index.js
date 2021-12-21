f05f652fae6ef9b76efebd7dcf85eafff0ebedb6
James Allardice <james@allardice.me>
2021-12-21 15:29:54 +0000

    Add missing export for SNS FIFO handlers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as handleJSON } from './json';
 export { default as responseJSON } from './json-response';
 export { default as handleSNS } from './sns';
+export { default as handleSNSFIFO } from './sns-fifo';
 export { default as handleSQSPartialBatchFailure } from './sqs-partial-batch-failure';
 export { default as handleURLEncoding } from './url-encoding';
