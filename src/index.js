150d1ad68e8bf01398b047d1e786524d5dd85be2
James Allardice <james@allardice.me>
2021-12-22 11:54:43 +0000

    Restrucure dir layout to prepare for addition of more tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-export { default as handleJSON } from './json';
-export { default as responseJSON } from './json-response';
-export { default as handleSNS } from './sns';
-export { default as handleSNSFIFO } from './sns-fifo';
-export { default as handleSQSPartialBatchFailure } from './sqs-partial-batch-failure';
-export { default as handleURLEncoding } from './url-encoding';
+// Lambda handlers. These are wrappers for Lambda functions themselves that
+// perform common logic such as request parsing.
+export { default as handleJSON } from './lambda-handlers/json';
+export { default as handleSNS } from './lambda-handlers/sns';
+export { default as handleSNSFIFO } from './lambda-handlers/sns-fifo';
+export { default as handleSQSPartialBatchFailure } from './lambda-handlers/sqs-partial-batch-failure';
+export { default as handleURLEncoding } from './lambda-handlers/url-encoding';
+
+// Lambda responses. These are utility functions for generating the return
+// values of Lambda functions.
+export { default as responseJSON } from './lambda-responses/json';
